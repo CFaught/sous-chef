b367f76f709fbac2be1ccb1e6a9f209568ebe307
Caleb Faught <faughtcj@gmail.com>
2016-12-10 19:36:47 -0600

    Implemented recipe index filtering with ajax

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -6,64 +6,43 @@ function Recipe(attributes) {
   this.ingredients = attributes.ingredients;
 }
 
+var ajaxResponse = function(data) {
+  var liString = "";
+
+  for (recipe of data) {
+    var tempRecipe = new Recipe(recipe);
+    if (currentUser.fav_ids().includes(tempRecipe.id)) {
+      tempRecipe.fav = "unfavorite";
+    } else {
+      tempRecipe.fav = "favorite";
+    }
+    liString += tempRecipe.renderLi();
+  }
+
+  $('.list-group').html(liString);
+}
+
 Recipe.prototype.renderLi = function () {
   return Recipe.template(this);
 };
 
 $(document).ready(function() {
-  // var header;
-  // var favoriteIds = [];
-
+  // Render list of recipes:
   Recipe.templateSource = $('#index-template').html();
   Recipe.template = Handlebars.compile(Recipe.templateSource);
 
+  $.get('/recipes.json').done(ajaxResponse); // end recipes index ajax
 
 
-  // $.get('/users/' + currentUserId + '/favorites_data').done(function(response) {
-  //   currentUser = response;
-  //   for ( element of currentUser.favorites ) {
-  //    favoriteIds.push(element.id);
-  //   }
-  // }); // end current_user favorites ajax
+  // Filter Recipes by ingredient
+  $('select').change(function(event) {
+    var ingredient = $(this).val();
 
-
-  $.get('/recipes.json').done(function(data) {
-    var liString = "";
-    // header = '';
-    // for (var index in data) {
-    //   header += '<div class="row-fluid">' +
-    //                  '<li class="list-group-item" style="height:50px;">' +
-    //                     '<div class="col-md-8">' +
-    //                       '<a href="/recipes/' + data[index].id + '" id="' + data[index].id + '" class="js-recipe-show">' + data[index].title + '</a>' +
-    //                     '</div>'
-    //   if (currentUser) {
-    //     if (favoriteIds.includes(data[index].id)) {
-    //       header  +=  '<div class="col-md-4">' +
-    //                     '<a href="/recipes/' + data[index].id + '/unfavorite">' + "Unfavorite" + '</a>' +
-    //                   '</div>'
-    //     } else {
-    //       header  +=  '<div class="col-md-4">' +
-    //                     '<a href="/recipes/' + data[index].id + '/favorite">' + "Favorite" + '</a>' +
-    //                   '</div>'
-    //     }
-    //   }
-    // }
-
-    // $('.list-group').html(
-    //       header +
-    //     '</li>' +
-    //   '</div>'
-    // )
-    for (recipe of data) {
-      var tempRecipe = new Recipe(recipe);
-      if (currentUser.fav_ids().includes(tempRecipe.id)) {
-        tempRecipe.fav = "unfavorite";
-      } else {
-        tempRecipe.fav = "favorite";
-      }
-      liString += tempRecipe.renderLi();
-    }
-
-    $('.list-group').html(liString);
-  }); // end recipes index ajax
+    $.ajax({
+      url: '/recipes.json',
+      method: 'GET',
+      contentType: "application/json; charset=utf-8",
+      data: {ingredient: ingredient}
+    }).done(ajaxResponse)
+  })
 });
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,15 @@
 class RecipesController < ApplicationController
   def index
+    @ingredients = Ingredient.all.to_a.uniq { |ingredient| ingredient.name.downcase }
+
+    # filter the @recipes list based on user input
+    if !params[:ingredient].blank?
+      @ingredient = Ingredient.find_by(id: params[:ingredient])
+      @recipes = Recipe.filter_by_ingredients(@ingredient)
+    else
+      # if no filters are applied, show all posts
+      @recipes = Recipe.order('recipes.title').all
+    end
     respond_to do |f|
       f.html { render :index }
       f.json { render json: @recipes }
